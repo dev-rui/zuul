c7b27265ccd282a187176c9ca12c1e1647b454e2
heruihong0325 <18655377896@163.com>
2019-12-17 20:50:18 +0800

    新增过滤器

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.example.apigateway;
 
+import com.example.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -13,4 +15,9 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
 }
